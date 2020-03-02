06682a5ff3aa65149d67466aff6f25fd2d38bf73
Fabrizio Ferreto <fabrizio.ferreto@gmail.com>
2020-03-02 17:31:48 -0600

    Inicio de interfaz
    Interfaz mas busqueda de archivos

---
+++ Nonogram/Assets/Scripts/BuscadorArchivos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+using UnityEngine.UI;
+
+public class BuscadorArchivos : MonoBehaviour
+{
+
+    public string dirExp;
+    public TMP_InputField txDir;
+    public Button boton;
+    public string direccion;
+
+
+    private void Update()
+    {
+        if (txDir.text != "")
+            boton.interactable = true;
+        else
+            boton.interactable = false;
+    }
+
+    public void AbrirExplorador()
+    {
+        dirExp = EditorUtility.OpenFilePanel("Nonogram a Resolver","","txt");
+        txDir.text = dirExp;
+    }
+
+    public void Confirmar()
+    {
+        direccion = txDir.text;
+        print(direccion);
+    }
+}
